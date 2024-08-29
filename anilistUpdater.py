523e6806604821d9bb712d5716555fb97236792b
AzuredBlue <holoadia11@gmail.com>
2024-08-29 13:39:41 +0200

    Fix format S0XE1 - {title} with name on folder

--- anilistUpdater.py
+++ anilistUpdater.py
@@ -153,63 +153,78 @@ def handle_filename(filename):
         filename = filename.split("\\")
 
     name = ""
+    season = ""
+    part = ""
+
     # Attempt to guess with the filename
     guess = guessit(filename[-1], {'type': 'episode'})
 
     # Debugging
-    print(guess)
+    print("File name guess: " + str(guess))
     keys = list(guess.keys())
 
+    # Episode guess from the title.
     # Usually, releases are formated [Release Group] Title - S01EX
+
     # If the episode index is 0, that would mean that the episode is before the title in the filename
     # Which is a horrible way of formatting it, so assume its wrong
-    # If its 1, then the title is probably 0, so its okay
+
+    # If its 1, then the title is probably 0, so its okay. (Unless season is 0)
+    # Really? What is the format "S1E1 - {title}"? That's almost psycopathic.
+
     # If its >2, theres probably a Release Group and Title / Season / Part, so its good
-    # Episode guess from the title.
 
     if "episode" in guess:
         episode = guess["episode"] # For cases in which the episode is 11.5, it will take it as episode 11 and 
                                    # therefore not updating it, since you watch episode 11 first.
         episode_index = keys.index("episode")
-        print("EPISODE INDEX: " + str(episode_index))
+        # print("EPISODE INDEX: " + str(episode_index))
     else:
         episode = 1 # If theres no episode count, assume 1.
         episode_index = 1 # If it has no episode, its probably a movie and it should be okay
     
+    # Store the "season" and "part" of the original file name if they have.
+    if "season" in guess:
+        season_index = keys.index("season")
+        season = str(guess["season"])
+
+    if "part" in guess:
+        part = str(guess["part"])
     
     # "Title" is the name's guess from guessit
-    if "title" in guess and episode_index > 0:
+    # If the episode index > 0 and season index > 0, its safe to assume that the title is in the file name
+    if "title" in guess and (episode_index > 0 and season_index > 0):
         name = guess["title"]
-        
-        # If there are any issues, try changing the name of the file using PowerRename (I can't express how good that program is)
-        if "season" in guess:
-            name = name + " " + str(guess["season"])
-
-        # If there is a part attached, append it to the name.
-        if "part" in guess:
-            name = name + " " + str(guess["part"])
-
     else:
         # If it isnt in the name of the file, try to guess using the name of the folder it is stored in
-        guess = guessit(filename[-2])
-        print(guess)
+        guess = guessit(filename[-2], {'type': 'episode'})
+        print("Folder guess:" + str(guess))
         name = guess["title"]
 
-        if "season" in guess:
-            name = name + " " + str(guess["season"])
+        # Add if they werent in the original file
+        if not season and "season" in guess:
+            season = str(guess["season"])
 
-        if "part" in guess:
-            name = name + " " + str(guess["part"])
+        if not part and "part" in guess:
+            part = str(guess["part"])
 
+    if season:
+        name = name + " " + season
+    
+    if part:
+        name = name + " " + part
+    
     # Adding the season AND part sometimes has its problems
     # See EIGHTY SIX SEASON 1 PART 2:
     # "EIGHTY SIX 1 2" does not give results searching, however
     # "EIGHTY SIX 2" does
 
+    print("Guessed name: " + name)
+
     # Get the id of the anime from AniList's api.
     anime_id = get_anime_id(name)
     
-    print(name)
+    
     # Increment the episode count
     increment_episode_count(anime_id, episode, name)
 
@@ -232,7 +247,6 @@ def get_anime_id(name):
 
     if response.status_code == 200:
         # Print the whole response for debugging.
-        print(response.json())
         return response.json()["data"]["Media"]["id"]
     else:
         raise Exception("Query failed!")
